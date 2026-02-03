678ad21e44fe34a4edf9e0565efb607e5ce66610
Hardik Pancha <43349475+hardikpancha@users.noreply.github.com>
2026-02-02 22:48:27 -0500

    Update DriverFactory.java

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -10,74 +10,66 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.CommonUtils;
 
 public class DriverFactory {
 
-//	private static WebDriver driver;
-	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private static WebDriver driver;
 
-	public static WebDriver initializeBrowser(String browserName) {
-		
-		boolean isCI = System.getenv("CI") != null;
+    public static WebDriver initializeBrowser(String browserName) {
+        boolean isCI = System.getenv("CI") != null;
 
-		if (browserName.equalsIgnoreCase("chrome")) {
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isCI) {
+                    chromeOptions.addArguments(
+                        "--headless=new",
+                        "--no-sandbox",
+                        "--disable-dev-shm-usage",
+                        "--disable-gpu",
+                        "--window-size=1920,1080"
+                    );
+                }
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
-			WebDriverManager.chromedriver().setup();
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isCI) {
+                    firefoxOptions.addArguments("--headless", "--width=1920", "--height=1080");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
 
-			ChromeOptions options = new ChromeOptions();
-			if (isCI) {
-				options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu",
-						"--window-size=1920,1080");
-			}
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if (isCI) {
+                    edgeOptions.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
+                }
+                driver = new EdgeDriver(edgeOptions);
+                break;
 
-			driver.set(new ChromeDriver(options));
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
 
-		} else if (browserName.equalsIgnoreCase("firefox")) {
+        // Common driver settings
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
 
-			WebDriverManager.firefoxdriver().setup();
+        return driver;
+    }
 
-			FirefoxOptions firefoxOptions = new FirefoxOptions();
-			if (isCI) {
-				firefoxOptions.addArguments("--headless");
-				firefoxOptions.addArguments("--width=1920");
-				firefoxOptions.addArguments("--height=1080");
-			}
+    public static WebDriver getDriver() {
+        return driver;
+    }
 
-			driver.set(new FirefoxDriver(firefoxOptions));
-
-		} else if (browserName.equalsIgnoreCase("edge")) {
-
-			WebDriverManager.edgedriver().setup();
-			EdgeOptions edgeOptions = new EdgeOptions();
-			if (isCI) {
-
-				edgeOptions.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
-			}
-
-			driver.set(new EdgeDriver(edgeOptions));
-
-		} else {
-			throw new IllegalArgumentException("Browser not supported: " + browserName);
-		}
-
-		driver.get().manage().deleteAllCookies();
-		driver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
-		driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-
-		return driver.get();
-	}
-
-	public static WebDriver getDriver() {
-		return driver.get();
-	}
-
-	// Quit and remove driver from ThreadLocal
-	public static void quitDriver() {
-		if (driver.get() != null) {
-			driver.get().quit();
-			driver.remove(); // important!
-		}
-	}
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
