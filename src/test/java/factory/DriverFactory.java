84c69a3ee7037b9596dd89a8ccad5c3dc7b94a69
Hardik Pancha <panchahardik2291@gmail.com>
2026-02-02 19:58:04 -0500

    Update DriverFactory: support Chrome, Firefox, Edge, Safari with headless where possible

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 import utils.CommonUtils;
@@ -16,28 +18,40 @@ public class DriverFactory {
 	static WebDriver driver = null;
 
 	public static WebDriver initializeBrowser(String browserName) {
+		
+		ChromeOptions options = new ChromeOptions();
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		EdgeOptions edgeOptions = new EdgeOptions();
 
 		if (browserName.equals("chrome")) {
 			driver = new ChromeDriver();
+			  // Required for GitHub Linux runners
+	        options.addArguments("--headless=new");        // Run headless
+	        options.addArguments("--no-sandbox");          // Disable sandbox
+	        options.addArguments("--disable-dev-shm-usage");// Avoid limited /dev/shm
+	        options.addArguments("--disable-gpu");         // Disable GPU
+	        options.addArguments("--window-size=1920,1080"); // Set window size
 
 		} else if (browserName.equals("firefox")) {
 			driver = new FirefoxDriver();
+            firefoxOptions.addArguments("--headless");           // Headless mode
+            firefoxOptions.addArguments("--window-size=1920,1080");
+            driver = new FirefoxDriver(firefoxOptions);
 
 		} else if (browserName.equals("edge")) {
 			driver = new EdgeDriver();
+			edgeOptions.addArguments("--headless=new");         // Headless mode
+            edgeOptions.addArguments("--disable-gpu");
+            edgeOptions.addArguments("--window-size=1920,1080");
+            driver = new EdgeDriver(edgeOptions);
 
 		} else if (browserName.equals("safari")) {
 			driver = new SafariDriver();
 		}
 
-        ChromeOptions options = new ChromeOptions();
+        
 
-        // Required for GitHub Linux runners
-        options.addArguments("--headless=new");        // Run headless
-        options.addArguments("--no-sandbox");          // Disable sandbox
-        options.addArguments("--disable-dev-shm-usage");// Avoid limited /dev/shm
-        options.addArguments("--disable-gpu");         // Disable GPU
-        options.addArguments("--window-size=1920,1080"); // Set window size
+      
 
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
